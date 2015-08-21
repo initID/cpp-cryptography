a33a453e8d6a4737bc68c26041b728a7d93d9533
ellivr <ellivr@users.noreply.github.com>
2015-08-22 01:08:11 +0700

    minor

--- cpp cryptography/Main.cpp
+++ cpp cryptography/Main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+
 #include "sha256.h"
 #include "sha384.h"
 #include "sha512.h"
@@ -11,9 +12,17 @@ int main()
 	cout << "Enter string: " << endl;
 
 	getline(cin, userInput);
-	output = sha512(userInput);
-
+	
+	output = sha256(userInput);
 	cout << "SHA256 hash of '" << userInput << "' is: " << endl;
+	cout << output << endl;
+
+	output = sha384(userInput);
+	cout << "SHA384 hash of '" << userInput << "' is: " << endl;
+	cout << output << endl;
+
+	output = sha512(userInput);
+	cout << "SHA512 hash of '" << userInput << "' is: " << endl;
 	cout << output << endl;
 
 	system("pause");
